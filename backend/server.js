c1425566693faf5abfbb2e3fc99799d516ae7b7a
Rodrigo Rodriguez <rod19016@byui.edu>
2022-07-07 00:31:03 -0600

    testing custom routes Auth0

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
+var axios = require('axios').default;
 const cors = require('cors');
 const express = require('express');
 const mongodb = require('./database/connect');
 const bodyParser = require('body-parser');
 const { auth, requiresAuth } = require('express-openid-connect'); // AUTHH0
 const swaggerUi = require('swagger-ui-express');
-// const swaggerDocumentAuto = require('./swagger/swagger-auto.json');
-const swaggerDocument = require('./swagger/swagger.json');
+const swaggerDocumentAuto = require('./swagger/swagger-auto.json');
+// const swaggerDocument = require('./swagger/swagger.json');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -18,13 +19,25 @@ const config = {
   baseURL: process.env.BASE_URL,
   // baseURL: process.env.BASE_URL_TEST,
   clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  // Source for custom routes (Section 3):
+  // https://github.com/auth0/express-openid-connect/blob/master/examples/custom-routes.js
+  routes: {
+    login: false,
+    postLogoutRedirect: '/custom-logout'
+  }
 };
 
 /* setup EXPRESS app */
 app
   /* AUTH0 validation */
+  // .use(auth(config))
   .use(auth(config))
+  .get('/profile', requiresAuth(), (req, res) => res.send(`hello ${req.oidc.user.sub}`))
+  .get('/login', (req, res) => res.oidc.login({ returnTo: '/profile' }))
+  .get('/custom-logout', (req, res) => res.oidc.logout({ returnTo: '/test' }))
+  .get('/', (req, res) => res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out'))
+  .get('/Test', (req, res) => res.send('Welcome to test'))
   .use(bodyParser.json())
   .use(cors())
   .use((req, res, next) => {
@@ -41,8 +54,8 @@ app
   .use('/', require('./routes'));
 
 /* SWAGGER documentation*/
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocumentAuto));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocumentAuto));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // to catch errors through the program
 process.on('uncaughtException', (err, origin) => {
